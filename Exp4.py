79ebd7f33a8a3ab96da86bb2f312d76be03ce07f
raveenadhakar <ravinadhakar351@gmail.com>
2026-01-28 11:16:03 +0530

    Exp4.py

---
+++ Exp4.py
@@ -0,0 +1,17 @@
+//EXP- 4.1
+
+my_list = [10, 20, 30, 40]
+my_tuple = (10, 20, 30, 40)
+print(&quot;List elements:&quot;)
+for item in my_list:
+print(item)
+print(&quot;Tuple elements:&quot;)
+for item in my_tuple:
+print(item)
+
+//EXP- 4.2
+
+num = int(input(&quot;Enter a number: &quot;))
+while num &gt;= 0:
+print(num)
+num -= 1
